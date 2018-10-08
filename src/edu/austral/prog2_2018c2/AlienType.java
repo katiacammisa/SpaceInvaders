c22c995ed7ee3604f4abb072320afd6dc37eee67
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2018-10-08 18:02:58 -0300

    AlienType

--- src/edu/austral/prog2_2018c2/AlienType.java
+++ src/edu/austral/prog2_2018c2/AlienType.java
@@ -5,7 +5,7 @@ public class AlienType {
    private int points;
    private String image;
 
-   public static AlienType BigAlien = new AlienType(10, "src/images/BigAlienAAAA.png");
+   public static AlienType BigAlien = new AlienType(10, "src/images/BigAlien.png");
    public static AlienType MediumAlien = new AlienType(20, "src/images/MediumAlien.png");
    public static AlienType SmallAlien = new AlienType(30, "src/images/SmallAlien.png");
    public static AlienType UFO = new AlienType((int) (Math.random() * 300) + 50, "src/images/alien.png");
