eadeeaec06c5f11dc348432d2c1d915c61ee4b40
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2018-10-09 22:50:42 -0300

    Nuevo random para ufo

--- src/edu/austral/prog2_2018c2/AlienType.java
+++ src/edu/austral/prog2_2018c2/AlienType.java
@@ -1,14 +1,18 @@
 package edu.austral.prog2_2018c2;
 
+import java.util.Random;
+
 public class AlienType {
 
    private int points;
    private String image;
+   private static Random rn = new Random();
+   private static int answer = rn.nextInt((3-1)+1) + 1;
 
    public static AlienType BigAlien = new AlienType(10, "src/images/BigAlien.png");
    public static AlienType MediumAlien = new AlienType(20, "src/images/MediumAlien.png");
    public static AlienType SmallAlien = new AlienType(30, "src/images/SmallAlien.png");
-   public static AlienType UFO = new AlienType((int) (Math.random() * 300) + 50, "src/images/alien.png");
+   public static AlienType UFO = new AlienType(answer, "src/images/alien.png");
 
     public AlienType(int points, String image) {
         this.points = points;
