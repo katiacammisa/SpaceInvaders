4a10c7f09c28583feeb4f77379bce5ff4f7c3ec5
Katy <katia.cammisa@ing.austral.edu.ar>
2018-10-09 22:39:54 -0300

    Pusheo cambios

--- src/edu/austral/prog2_2018c2/Board.java
+++ src/edu/austral/prog2_2018c2/Board.java
@@ -198,7 +198,7 @@ public class Board extends JPanel implements Runnable, Commons {
 
         Image cat;
 
-        if(message.equals("Game Over")) {
+        if(message.equals("Game Over :(")) {
             cat = new ImageIcon("src/images/gatito.jpg").getImage();
             g.drawImage(cat, 0, 0, null);
         } else {
